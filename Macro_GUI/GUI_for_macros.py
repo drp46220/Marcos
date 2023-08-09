a48850a99284eb3e2c96ae445178cd2ba2656e62
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-08-08 23:35:30 -0500

    all funtions have buttons

--- Macro_GUI/GUI_for_macros.py
+++ Macro_GUI/GUI_for_macros.py
@@ -4,15 +4,16 @@ from tkinter import *
 from tkinter import ttk
 from GUI_Music import *
 from GUI_Social import *
+from GUI_VSC import *
 
 def MainWindow():
     Main_Window = tkinter.Tk()
     Main_Window.title('Macro GUI')
     Frame = ttk.Frame(Main_Window, padding=10)
     Frame.grid()
-    # clicked = ttk.IntVar()
     MusicBtn = ttk.Button(Frame, text="Music", command=lambda: Object.openAudio()).grid(column=1, row=0)
     SocialBtn = ttk.Button(Frame, text="Social", command=lambda: Object.openDiscord()).grid(column=1, row=1)
+    EditorBtn = ttk.Button(Frame, text="Editor", command=lambda: Object.openVSC()).grid(column=1, row=2)
     Main_Window.mainloop()
 
 if __name__ == "__main__":
